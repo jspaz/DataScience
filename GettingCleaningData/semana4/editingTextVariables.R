ab29b6a69185033142e840fbb230ad4fc0fe25ba
jspaz <paz.sergio@gmail.com>
2016-06-29 19:34:54 -0500

    nuevos ejemplos

--- GettingCleaningData/semana4/editingTextVariables.R
+++ GettingCleaningData/semana4/editingTextVariables.R
@@ -7,4 +7,49 @@ dateDownloaded <- date()
 list.files("./data")
 cameraData <- read.csv("./data/cameras.csv")
 names(cameraData)
+# Poner los caracteres de un vector en minúsculas
 tolower(names(cameraData))
+# Poner los caracteres de un vector en mayúsculas
+toupper(names(cameraData))
+# Separar los nombres de las variables
+splitNames = strsplit(names(cameraData), "\\.")
+# Crear listas aparte
+mylist <- list(letters = c("A", "b", "c"), numbers = 1:3, matrix(1:25, ncol = 5))
+head(mylist)
+mylist[1]
+mylist$letters
+mylist[[1]]
+# Aplicar una función a cada uno de los elementos de un vector o una lista
+splitNames[[6]][1]
+firstElement <- function(x){x[1]} # La función obtiene el primer elemento de la lista de elementos
+sapply(splitNames, firstElement)
+# Obtención de datos de prueba
+library(curl)
+fileUrl1 <- "https://dl.dropboxusercontent.com/u/7710864/data/reviews-apr29.csv"
+fileUrl2 <- "https://dl.dropboxusercontent.com/u/7710864/data/solutions-apr29.csv"
+download.file(fileUrl1, destfile = "./data/reviews.csv", method = "auto")
+download.file(fileUrl2, destfile = "./data/solutions.csv", method = "auto")
+reviews <- read.csv("./data/reviews.csv"); solutions <- read.csv("./data/solutions.csv")
+head(reviews, 2)
+head(solutions, 2)
+names(reviews)
+# Se sustituyen los nombres de las variables que tienen guión bajo por nada
+sub("_", "", names(reviews))
+# Remplazar múltiples instancias de un caracter en particular
+testName <- "this_is_a_test"
+sub("_", "", testName) # Remplaza sólo el primer guión bajo en la variable
+gsub("_", "", testName) # Remplaza todos los guiones bajos de la variable
+# Buscar valores específicos en el nombre de las variables o dentro de la variable
+grep("Alameda", cameraData$intersection) # Muestra las posiciones en las que se encuentra la coincidencia exacta
+table(grepl("Alameda", cameraData$intersection)) # Muestra en una tabla con el total de elementos que coinciden y no coinciden
+cameraData2 <- cameraData[!grepl("Alameda", cameraData$intersection),] # Crea un subconjunto en donde con los datos en donde no se encuentre el valor Alameda
+grep("Alameda", cameraData$intersection, value = TRUE) # Muestra los valores en donde se encuentran las coincidencias
+grep("JeffStreet", cameraData$intersection) # Revisar si un valor no aparece en el vector (total de veces)
+length(grep("JeffStreet", cameraData$intersection)) # Otra forma de revisar si no existe el valor (posiciones)
+library(stringr)
+nchar("Jeffrey Leek") # Cuenta el número de caracteres, incluyendo espacios
+substr("Jeffrey Leek", 1, 7) # Sólo toma los caracteres del 1 al 7
+paste("Jeffrey", "Leek") # Pegar dos cadenas de caracteres separados por un espacio
+paste("Jeffrey", "Leek", sep = "--") # Pega dos cadenas separados por --
+paste0("Jeffrey", "Leek") # Pega dos cadenas sin espacios de separación
+str_trim("Jeff - _       ") # Elimina los espacios extras al final de la cadena de caracteres
---
+++ GettingCleaningData/semana4/regularExpressions_I-II.R
@@ -0,0 +1,5 @@
+# El metacaracter ^ representa el inicio de una línea: ^inicio
+# El metacaracter $ representa el final de una línea: final$
+# [] se consideran para listar un conjunto de caracteres: [Bb][Us][[Ss]][[Hh]]
+# [] se pueden especificar un rango de letras: ^[0-9][a-zA-Z]
+# [] indica un conjunto de caracteres no incluidos: [^?.]$ 
