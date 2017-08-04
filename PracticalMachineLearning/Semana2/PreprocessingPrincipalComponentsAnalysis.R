b65a20f87239da24febfac606de5c61e762ce82f
jspaz <paz.sergio@gmail.com>
2017-08-03 22:35:34 -0500

    ejemplos semana2

---
+++ PracticalMachineLearning/Semana2/PreprocessingPrincipalComponentsAnalysis.R
@@ -0,0 +1,45 @@
+#Correlated predictors
+library(caret); library(kernlab); data("spam")
+inTrain <- createDataPartition(y=spam$type, p=0.75, list = FALSE)
+training <- spam[inTrain,]
+testing <- spam[-inTrain,]
+
+M <- abs(cor(training[,-58]))
+diag(M) <- 0
+which(M > 0.8, arr.ind = T)
+
+names(spam)[c(34, 32)]
+plot(spam[,34],spam[,32])
+
+#Rotar la gráfica
+X <- 0.71*training$num415 + 0.71*training$num857
+Y <- 0.71*training$num415 - 0.71*training$num857
+plot(X, Y)
+
+#Usando R - prcomp
+smallSpam <- spam[,c(34, 32)]
+prComp <- prcomp(smallSpam)
+plot(prComp$x[,1], prComp$x[,2])
+prComp$rotation
+
+#PCA con datos SPAM
+typeColor <-((spam$type=="spam")*1+1)
+prComp <- prcomp(log10(spam[,-58]+1))
+plot(prComp$x[,1],prComp$x[,2], col=typeColor,xlab="PC1",ylab="PC2")
+
+#PCA usando Caret
+preProc <- preProcess(log10(spam[,-58]+1), method = "pca", pcaComp = 2)
+spamPC <- predict(preProc, log10(spam[,-58]+1))
+plot(spamPC[,1], spamPC[,2], col=typeColor)
+
+#Preproceso con PCA
+preProc <- preProcess(log10(training[,-58]+1), method = "pca", pcaComp = 2)
+trainPC <- predict(preProc, log10(training[,-58]+1))
+modelFit <- train(training$type ~ .,method="glm", data = trainPC)
+testPC <- predict(preProc, log10(testing[,-58]+1))
+confusionMatrix(testing$type, predict(modelFit, testPC))
+
+#Alternativa
+modelFit <- train(training$type ~., method='glm', preProcess='pca', data = training)
+confusionMatrix(testing$type, predict(modelFit,testing))
+
---
+++ PracticalMachineLearning/Semana2/predictingRegressionMultipleCovariates.R
@@ -0,0 +1,44 @@
+# Cargar datos
+library(ISLR); library(ggplot2); library(caret)
+data("Wage"); Wage <- subset(Wage, select = -c(logwage))
+summary(Wage)
+
+# Entrenamiento y pruebas
+inTrain <- createDataPartition(y=Wage$wage, p=0.7, list = FALSE)
+training <- Wage[inTrain,]; testing <- Wage[-inTrain,]
+dim(training); dim(testing)
+
+# Graficar
+featurePlot(x=training[,c("age", "education", "jobclass")], y=training$wage, plot = "pairs")
+
+# Gráfica edad vs salario
+qplot(age, wage, data = training)
+
+# Gráfica identificando el tipo de trabajo
+qplot(age, wage, colour=jobclass, data = training)
+
+# Gráfica identificando la educación
+qplot(age, wage, colour=education, data = training)
+
+# Ajustar el modelo lineal
+modFit <- train(wage ~ age + jobclass + education, method="lm", data = training)
+finMod <- modFit$finalModel
+print(modFit)
+
+# Gráfica de diagnóstico
+plot(finMod, 1, pch=19, cex=0.5, col="#00000010")
+
+# Variables no utilizadas en el modelo
+qplot(finMod$fitted, finMod$residuals, colour=race, data = training)
+
+# Gráfica por índice
+plot(finMod$residuals, pch=19)
+
+# Predicción vs verdad
+pred <- predict(modFit, testing)
+qplot(wage, pred, colour=year, data = testing)
+
+# Utilizar todas las covariantes
+modFitAll <- train(wage ~., data = training, method="lm")
+pred <- predict(modFitAll, testing)
+qplot(wage, pred, data = testing)
